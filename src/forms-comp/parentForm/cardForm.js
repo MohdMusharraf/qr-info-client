5c12d7e1695716c07ad24f2136224d3dd614bdd7
Mohd Musharraf <codejam9888@gmail.com>
2023-08-16 21:31:15 +0530

    previous version

--- src/forms-comp/parentForm/cardForm.js
+++ src/forms-comp/parentForm/cardForm.js
@@ -54,7 +54,7 @@ export default function CardForm() {
             note
         } = formData;
     
-        const res = await fetch('https://qrinfo.onrender.com/users', {
+        const res = await fetch('/users', {
             method: "POST",
             headers: {
             "Content-Type": "application/json"
